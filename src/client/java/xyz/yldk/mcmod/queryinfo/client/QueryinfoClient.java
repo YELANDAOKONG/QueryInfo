b939f73ee92e189f659a52b0434d835d6f9653ea
YELANDAOKONG <yelandaokong@yldk.xyz]>
2025-02-16 04:13:28 +0800

    Init

---
+++ src/client/java/xyz/yldk/mcmod/queryinfo/client/QueryinfoClient.java
@@ -0,0 +1,10 @@
+package xyz.yldk.mcmod.queryinfo.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class QueryinfoClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/xyz/yldk/mcmod/queryinfo/Queryinfo.java
@@ -0,0 +1,10 @@
+package xyz.yldk.mcmod.queryinfo;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Queryinfo implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
